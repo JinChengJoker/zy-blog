2618fe38f3b1ada00dbae2578800e31a2d62a3b5
JinCheng <jinchengjoker@foxmail.com>
2020-07-03 18:25:47 +0800

    SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,36 @@
 import Link from "next/link";
 import React from "react";
+import {GetServerSideProps} from "next";
+import {UAParser} from 'ua-parser-js'
 
-export default function Home() {
+type Props = {
+  browser: {
+    name: string
+    version: string
+    major: string
+  }
+}
+
+export default function Home(props: Props) {
+  const {browser} = props
   return (
     <div className="container">
       <main>
         <h1 className="title">
-          点击访问 <Link href="/posts/first-post"><a>第一篇博客</a></Link>
+          <Link href="/posts"><a>博客列表</a></Link>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <p>当前浏览器 {browser.name}</p>
       </main>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const ua = context.req.headers["user-agent"]
+  const uaResult = new UAParser(ua).getResult()
+  return {
+    props: {
+      browser: uaResult.browser
+    }
+  }
 }
